50e34d9f50f89722ce36681f4248e733231ea006
AxelCruzado <95115663+AxelCruzado@users.noreply.github.com>
2025-11-03 17:27:49 -0500

    ADD

--- api/secciones/nacional.php
+++ api/secciones/nacional.php
@@ -4,9 +4,9 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $nacional = [
-"titulo" => "Guillermo Bermejo recluido en el penal Ancón I: ¿Qué otros juicios y procesos afrontará tras su sentencia por terrorismo?",
-"autor" => "Karem Barboza Quiroz",
-"resumen" => "Bermejo cumplirá en el penal Ancón I su condena de 15 años de prisión por afiliación a la organización terrorista Sendero Luminoso.",
-"image" => "/imagenes/bermejo_carcel.avif",
+"titulo" => "Keiko Fujimori anuncia su candidatura por cuarta vez a la presidencia de Perú en 2026",
+"autor" => "CNN Latinoamérica",
+"resumen" => "La fundadora del partido Fuerza Popular, Keiko Fujimori, anunció este jueves ante sus seguidores en Trujillo que se postulará por cuarta vez a la presidencia de Perú, el cargo que su padre Alberto Fujimori ocupó cuando ella tenía 15 años.",
+"image" => "/imagenes/keikopostula.jpg",
 ];
 ?>
