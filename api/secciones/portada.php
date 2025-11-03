be6b4531fb60eabfd17f62b890803bc7b3a0143b
AxelCruzado <95115663+AxelCruzado@users.noreply.github.com>
2025-11-03 17:15:38 -0500

    solution

--- api/secciones/portada.php
+++ api/secciones/portada.php
@@ -4,7 +4,7 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $portada = [
-"titulo" => "Portada",
+"titulo" => "Axel Cruzado Alegre - Noticiero",
 "autor" => "AxelCruzado",
 "imagen" => "imagenes/portada.webp",
 "resumen" => "Las noticias mas importantes del 2025",
